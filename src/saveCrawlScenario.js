39535fbcbaef8d0e062f31b6a9d6a80aef4d3284
chenweiLabri <888chenwei666@gmail.com>
2018-01-02 16:48:19 +0100

    bug of saveCrawlScenario

--- src/saveCrawlScenario.js
+++ src/saveCrawlScenario.js
@@ -1,36 +1,32 @@
-const wat_action = require('wat_action_nightmare');
-const request = require('request');
-const Promise = require('promise');
 const request_promise = require('request-promise');
+const requestUrl = 'http://localhost';
 
-function saveCrawlScenario(baseScenario){
+async function crawlScenario(baseScenario){
 	var baseScenarioActions = baseScenario.actions;
 	var bid = baseScenario._id;
+
 	for (var i = 0; i <= baseScenarioActions.length - 1; i++) {
-		var newActions = baseScenarioActions.slice(0, i + 1);
-		var scenario = new wat_action.Scenario(newActions);		
-		var scenarioActions = JSON.parse(scenario.toJSON());
-		postToSaveCrawlScenario(scenarioActions, bid);
+		var newActions = await baseScenarioActions.slice(0, i + 1);
+		var sce = {"actions" : newActions, "bid" : bid};
+		await sendCrawlScenarioToRabbit(sce);
 		
 	}
 }
 
-function postToSaveCrawlScenario(scenarioActions, bid){
+function sendCrawlScenarioToRabbit(sce){
 	request_promise({
 		method: 'POST',
-		uri: 'http://localhost:8086/crawlScenario/',
-		body: { "scenarioActions": scenarioActions , "bid": bid },
+		uri: requestUrl + ':8091/crawlNow/',
+		body: sce,
 		json: true
 	})
-    .then(function (parsedBody) {// POST succeeded...
-    	console.log(parsedBody);
-    	return Promise.resolve(parsedBody);
-
-    })
-    .catch(function (err) {// POST failed...
-    	console.log(err);
-    	return Promise.reject(err);
-    });
+	.then(function (parsedBody) {
+		// return Promise.resolve(parsedBody);
+	})
+	.catch(function (err) {
+		console.log(err);
+		// return Promise.reject(err);
+	});
 }
 
-exports.saveCrawlScenario = saveCrawlScenario;
+exports.crawlScenario = crawlScenario;
