1fc3edc867a8cc8e719a8970f314d9d6ed79aa6d
chenweiLabri <888chenwei666@gmail.com>
2018-01-02 16:54:53 +0100

    save baseAction

--- src/saveBaseActions.js
+++ src/saveBaseActions.js
@@ -1,28 +1,20 @@
-const request = require('request');
-const Promise = require('promise');
 const request_promise = require('request-promise');
 
-function saveBaseActions(baseScenarioActions){//input baseScenarionActions and save all the actions to mongodb table action
+async function saveBaseActions(baseScenarioActions){//input baseScenarionActions and save all the actions to mongodb table action
 	for (var i = 0; i <= baseScenarioActions.length - 1; i++) {
-		saveOneBaseAction(baseScenarioActions[i]);
+		await request_promise({
+			method: 'POST',
+			uri: 'http://localhost:8086/action/',
+			body: baseScenarioActions[i],
+			json: true
+		})
+	    .then(function (parsedBody) {// POST succeeded...
+	    	return Promise.resolve(parsedBody);
+	    })
+	    .catch(function (err) {// POST failed...
+	    	console.log(err);
+	    	return Promise.reject(err);
+	    });
 	}
-	// return Promise.resolve();
 }
-
-function saveOneBaseAction(baseActionJson){
-	request_promise({
-		method: 'POST',
-		uri: 'http://localhost:8086/action/',
-		body: baseActionJson,
-		json: true
-	})
-    .then(function (parsedBody) {// POST succeeded...
-    	return Promise.resolve(parsedBody);
-    })
-    .catch(function (err) {// POST failed...
-    	console.log(err);
-    	return Promise.reject(err);
-    });
-}
-
-exports.saveBaseActions = saveBaseActions;
+module.exports.saveBaseActions = saveBaseActions;
