62844a69bacf504d900c6d54970ca34270fa2809
chenweiLabri <888chenwei666@gmail.com>
2018-01-19 16:18:42 +0100

    choose one noise for each location

--- src/Noise.js
+++ src/Noise.js
@@ -6,20 +6,56 @@ const requestUrl = 'http://localhost';
 
 class Noise{
 
-	constructor(noiseNum){
-		this.noiseNum = noiseNum;
+	constructor(baseScenario,locations){
+		this.baseScenario = baseScenario;
+		this.locations = locations;
+		this.stepNoises = [];
 	}
 
-	//input increaseLength of scenario === the number of noise action === num of location
+	// sort by probability value from high to low
+	compare(a, b) {
+		return b.probability - a.probability;
+	};
+
+	//input baseScenario and locations, 
+	//for each location, search all candidates and choose one according probability
+	async getOneStepEachLocation(){
+		for (var i = 0; i < this.locations.length; i++) {			
+			var reqParams = {preIndex: this.locations[i], bid: this.baseScenario._id};
+			var stepResult = await this.requestGetSteps(reqParams);
+			await this.stepNoises.push(stepResult);			
+		}
+		return this.stepNoises;
+	}
+
+	async requestGetSteps(reqParams){
+		return new Promise(async (resolve, reject) => {
+			var options = {  
+				url: requestUrl + ':8086/step/preIndex/',
+				body: reqParams,
+				method: 'GET',
+				json:true
+			};
+			await request.get(options, async (error, response, body) => {
+				if (!error) {
+					var tempList = await arrayShuffle(body).sort(this.compare);					
+					resolve(tempList[0]);
+				} else {
+					reject(error);
+				}
+			});
+		});
+	}
+
+	//input increaseLength of scenario === the number of noise action === num of locations
 	//get all the stepActions from mongo
 	//choose the steps by probability value from high to low
 	async getAndChooseStep(){
-
-		var promise = new Promise(async (resolve, reject) => {
+		return new Promise(async (resolve, reject) => {
 			request.get(requestUrl + ':8086/step/', async (error, response, body) => {
 				if (!error) {
 					var steps = await JSON.parse(body);
-					var tempList = await arrayShuffle(steps).sort(this.compare).splice(0, this.noiseNum);
+					var tempList = await arrayShuffle(steps).sort(this.compare).splice(0, this.locations.length);
 					var chosenStep = await arrayShuffle(tempList);
 					resolve(chosenStep);
 				} else {
@@ -28,14 +64,7 @@ class Noise{
 				}
 			});
 		});
-
-		return promise;
 	}
-
-	// sort by probability value from high to low
-	compare(a, b) {
-		return b.probability - a.probability;
-	};
 
 }
 
